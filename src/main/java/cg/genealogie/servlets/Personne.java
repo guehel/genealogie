fa79ecce10399a4103c6eb6493c6463fbcf6e3cb
guehel <guehel@yahoo.fr>
2014-08-03 20:47:07 -0400

    debut de modif de la page personne

--- src/main/java/cg/genealogie/servlets/Personne.java
+++ src/main/java/cg/genealogie/servlets/Personne.java
@@ -10,14 +10,17 @@ import cg.genealogie.servlets.traitments.Traitement;
 import cg.genealogie.servlets.traitments.TraitementPersonne;
 
 public class Personne extends HttpServlet {
-	public static String urlPagePersonne ="";
-public void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException{
-	
+	public static String urlPagePersonne ="/WEB-INF/Personne.jsp";
+	/*Traite les demande de connection a la page /genealogie/personne*/
+public void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException{
+	this.getServletContext().getRequestDispatcher(urlPagePersonne).forward(request, response);
 }
+/*Traite les sauvegarde en post de la page /genealogie/personne*/
 public void doPost(HttpServletRequest request , HttpServletResponse response)throws ServletException, IOException{
 Traitement traitementPersonne = new TraitementPersonne();
 Boolean reussite  = traitementPersonne.traiter(request);
 request.setAttribute("reussite", reussite);
+System.out.println(reussite);
 this.getServletContext().getRequestDispatcher(urlPagePersonne).forward(request, response);
 }
 }
